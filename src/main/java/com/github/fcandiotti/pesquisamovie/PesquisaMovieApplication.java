8b9acd1b15555525391e9f86350291ac6f8f32f1
fcandiotti <fernando_candiotti@hotmail.com>
2022-02-03 05:21:05 -0300

    projeto iniciado

---
+++ src/main/java/com/github/fcandiotti/pesquisamovie/PesquisaMovieApplication.java
@@ -0,0 +1,13 @@
+package com.github.fcandiotti.pesquisamovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisaMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PesquisaMovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/fcandiotti/pesquisamovie/PesquisaMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fcandiotti.pesquisamovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisaMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
