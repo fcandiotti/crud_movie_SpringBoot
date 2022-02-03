6952b61f8768522bc8837473b4cea6ba775c228d
fcandiotti <fernando_candiotti@hotmail.com>
2022-02-03 09:43:28 -0300

    identacao

--- src/main/java/com/github/fcandiotti/pesquisamovie/repositories/RecordRepository.java
+++ src/main/java/com/github/fcandiotti/pesquisamovie/repositories/RecordRepository.java
@@ -13,8 +13,8 @@ import java.time.Instant;
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
-    @Query("SELECT obj FROM Record obj WHERE " +
-            "(:minDate IS NULL OR obj.moment >= :minDate) AND " +
-            "(:maxDate IS NULL OR obj.moment <= :maxDate)")
+    @Query("SELECT obj FROM Record obj WHERE "
+            + "(:minDate IS NULL OR obj.moment >= :minDate) AND "
+            + "(:maxDate IS NULL OR obj.moment <= :maxDate)")
     Page<Record> findByMoments(Instant minDate, Instant maxDate, Pageable pageable);
 }
